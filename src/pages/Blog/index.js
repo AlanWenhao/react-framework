f27340a235a35ae35e552e70519047cb88c46b86
AlanWenhao <13454782783@163.com>
2018-03-16 11:08:38 +0800

    init base code

---
+++ src/pages/Blog/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Blog extends Component {
+    render() {
+        return (
+            <div>这里是Blog Page~~</div>
+        );
+    }
+}
+
+export default Blog;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>这里是Home Page~~</div>
+        );
+    }
+}
+
+export default Home;
